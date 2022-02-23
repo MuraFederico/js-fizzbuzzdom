56d8a2a9f99c516637ad89e13a17f2480985160e
MuraFederico <federicomura98@gmail.com>
2022-02-23 16:04:59 +0100

    rimosso completamente lo 0 in quanto non voluto dalla consegna

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 let container = document.querySelector('.container');
 
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     let box = document.createElement('div');
     container.append(box);
     box.classList.add('box');
     // console.log(box)
-    if (i % 15 == 0 && i / 1 != 0){
+    if (i % 15 == 0){
         box.classList.add('fizz-buzz');
         box.append('fizzbuzz');
     }
-    else if( i % 3 == 0 && i / 1 != 0){
+    else if( i % 3 == 0){
         box.classList.add('fizz');
         box.append('fizz');
     }
-    else if(i % 5 == 0 && i / 1 != 0){
+    else if(i % 5 == 0){
         box.classList.add('buzz');
         box.append('buzz');
     }
